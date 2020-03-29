c4e0446420d23007e02911466b2e682385f71837
eyal arasu <elarasu@gmail.com>
2020-03-28 20:27:37 -0700

    typed values in log

--- log.go
+++ log.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	log "github.com/elarasu/basis/log"
+	"go.uber.org/zap"
 )
 
 func init() {
@@ -22,18 +23,21 @@ func main() {
 	log.Debug("Sample debug statement")
 	// use this format for non-performant logs
 	logger := log.Sugar()
+	// sugar api for just k,v
 	logger.Infow("failed to fetch URL",
 		"url", "http://example.com",
 		"attempt", 3,
 		"backoff", time.Second,
 	)
+	// printf style
 	logger.Infof("failed to fetch URL %s", "http://sample.com")
-	// uncomment - use the typed fields for performant needs
-	// log.Info("failed to fetch URL",
-	// 	zap.String("url", "http://example.com"),
-	// 	zap.Int("attempt", 3),
-	// 	zap.Duration("backoff", time.Second),
-	// )
+	// use the typed fields for performant needs
+	log.Info("failed to fetch URL",
+		zap.String("url", "http://example.com"),
+		zap.Int("attempt", 3),
+		zap.Duration("backoff", time.Second),
+	)
+	// for just string use direct log
 	log.Warn("Warn a situation")
 	log.Fatal("Fatal, can't proceed further")
 	// this statement wouldn't be reached
